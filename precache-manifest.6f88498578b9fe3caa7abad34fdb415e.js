e6b74b51115b5ceb8dbd1aa5b2fe404c3ee806ed
tumzied <abdur963rahman@gmil.com>
2020-06-16 21:19:57 +0600

    Updates

---
+++ precache-manifest.6f88498578b9fe3caa7abad34fdb415e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e5babc4015969f3b104f68272f79bf1",
+    "url": "/gradienttools/index.html"
+  },
+  {
+    "revision": "970d4f068301d4bb5bcc",
+    "url": "/gradienttools/static/css/main.c63bf52c.chunk.css"
+  },
+  {
+    "revision": "f7f71b499408c2692c0c",
+    "url": "/gradienttools/static/js/2.1b313046.chunk.js"
+  },
+  {
+    "revision": "970d4f068301d4bb5bcc",
+    "url": "/gradienttools/static/js/main.70cef439.chunk.js"
+  },
+  {
+    "revision": "0e4ae5074d4f5e124dd4",
+    "url": "/gradienttools/static/js/runtime~main.04ffaf79.js"
+  }
+]);
